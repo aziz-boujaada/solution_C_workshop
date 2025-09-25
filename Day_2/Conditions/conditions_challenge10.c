99576ba77099b41372ea7586e4a2cdbc312316f3
aziz_boujaada <boujaadaaziz2911@gmail.com>
2025-09-25 11:06:32 +0100

    day 2 challenges up to challenge 10

---
+++ Day_2/Conditions/conditions_challenge10.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    printf(" int  = %d\n",sizeof(int));
+    printf(" int  = %d\n",sizeof(short));
+    printf(" int  = %d\n",sizeof(long));
+    printf(" int  = %d\n",sizeof(double));
+    printf(" int  = %d\n",sizeof(float));
+    printf(" int  = %d\n",sizeof(long double));
+    return 0;
+}
